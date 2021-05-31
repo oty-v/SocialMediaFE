306fe9c01be5e60e969de88bbcc86d582c115cc8
Oty_v <oty998@gmail.com>
2021-05-31 17:00:05 +0300

    edit src

--- components/users/userAvatar.js
+++ components/users/userAvatar.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const UserAvatar = ({userAvatar, width, height}) => {
-    const src = !!userAvatar ? userAvatar : '/default.png'
+    const src = userAvatar || '/default.png'
     return (
         <Image
             src={src}
