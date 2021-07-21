900b6b1db3ac31c675eb18f425d84674d73f9f18
KarevichMaksim <karevichm@gmail.com>
2021-07-21 17:08:36 +0300

    added pusher notifications

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     env: {
         PORT: 3000,
-        API_URL: 'http://127.0.0.1:8000/api'
+        API_URL: 'http://127.0.0.1:8000/api',
+        PUSHER_APP_ID: '1238148',
+        PUSHER_APP_KEY: '101e1ae6cb9852ca18e4',
+        PUSHER_APP_SECRET: '7e8630a88b4408a9f9e3',
+        PUSHER_APP_CLUSTER: 'eu',
     },
     images: {
         domains: ['127.0.0.1'],
